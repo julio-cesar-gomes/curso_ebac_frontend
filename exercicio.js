cf93f50eb04cb675c48c550ceab4c8519095e394
Julio Gomes <103766428+julio-cesar-gomes@users.noreply.github.com>
2023-09-15 22:29:12 -0300

    primeiro commit

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function mutiplicador(num1, num2) {
+    return num1 * num2;
+}
+function DizOi(nome) {
+    return `Olá ${nome}`;
+}
+console.log(DizOi('Ana'));
+console.log(mutiplicador(2, 4));
---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function mutiplicador(num1: number, num2: number) {
+    return num1 * num2;
+}
+
+function DizOi(nome: string){
+    return `Olá ${nome}`
+}
+
+console.log(DizOi('Ana'));
+console.log(mutiplicador(2,4))
